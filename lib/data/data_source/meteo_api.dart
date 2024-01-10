0c9f4bf15837f0d55977ccc905b32aa836efe1d7
kimkangtae <tt997768@gmail.com>
2024-01-10 16:00:45 +0900

    Feat: Api 호출 파라미터 수정

--- lib/data/data_source/meteo_api.dart
+++ lib/data/data_source/meteo_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class MeteoApi {
   Future<MeteoDto> getWeatherResult(double latitude, double longitude) async {
     final response = await http.get(Uri.parse(
-        'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=$latitude&longitude=$longitude'));
+        'https://api.open-meteo.com/v1/forecast?daily=temperature_2m_max,temperature_2m_min,weathercode&latitude=$latitude&longitude=$longitude'));
     return MeteoDto.fromJson(jsonDecode(response.body));
   }
 }
