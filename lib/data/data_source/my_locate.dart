6b8998195f8b11efdba581a96a72ef769aae3847
kimkangtae <tt997768@gmail.com>
2024-01-10 18:57:00 +0900

    Feat: 권한관련 수정

--- lib/data/data_source/my_locate.dart
+++ lib/data/data_source/my_locate.dart
@@ -5,21 +5,20 @@ class MyLocation {
   late double latitude2;
   late double longitude2;
 
-  Future<void> getMyCurrentLocation() async {
+  Future<void> getCurrentLocation() async {
 
-    // 위치권한을 가지고 있는지 확인
-    var status_position = await Permission.location.status;
-
-    if (status_position.isGranted) {
-      // 1-2. 권한이 있는 경우 위치정보를 받아와서 변수에 저장합니다.
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
+    LocationPermission permission = await Geolocator.checkPermission();
+    // print(permission);
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       latitude2 = position.latitude;
       longitude2 = position.longitude;
-
-    } else {
-      // 1-3. 권한이 없는 경우
-      print("위치 권한이 필요합니다.");
+    } catch (e) {
+      print(e);
     }
   }
 }
